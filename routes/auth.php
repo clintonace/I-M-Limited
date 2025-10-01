ff42642568703889fde45c397192de10b97b739f
clintonace <clintonace09@gmail.com>
2025-10-01 09:59:04 +0100

    attack

--- routes/auth.php
+++ routes/auth.php
@@ -16,7 +16,7 @@ Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('register', [RegisteredUserController::class, 'store'])->middleware('throttle:1,10');
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
